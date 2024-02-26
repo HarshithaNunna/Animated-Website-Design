d47e4917a825f1ec8229a27fd7ffd76069b6b768
HarshithaNunna <98328061+HarshithaNunna@users.noreply.github.com>
2024-02-27 00:20:55 +0530

    Create website.js

---
+++ website.js
@@ -0,0 +1,9 @@
+/*smooth scrolling - we use locomotive js
+    steps-
+        attach loco scroll css
+        attach locomotive scroll min js
+        some code from loco github for js
+  gsap
+    steps-
+        attach gsap
+  scrolltrigger*/
